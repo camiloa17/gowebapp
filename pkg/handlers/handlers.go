9d8a6103bb65512062e4b9dee1df030acd8fc537
Camilo <camiloa17@gmail.com>
2023-04-21 22:25:57 -0700

    Add more of session

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -24,22 +24,27 @@ func NewRepo(app *config.AppConfig) *Repository {
 }
 
 // NewHandlers sets the repository for the handlers
-func NewHandlers(r *Repository) {
-	Repo = r
+func NewHandlers(rs *Repository) {
+	Repo = rs
 }
 
 // Home is the home page handler
-func (r *Repository) Home(responseWriter http.ResponseWriter, request *http.Request) {
+func (rs *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
 
-	render.RenderTemplate(responseWriter, "home.page.gohtml", &models.TemplateData{})
+	rs.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+	render.RenderTemplate(w, "home.page.gohtml", &models.TemplateData{})
 }
 
 // About is the about page handler
-func (r *Repository) About(responseWriter http.ResponseWriter, request *http.Request) {
+func (rs *Repository) About(responseWriter http.ResponseWriter, r *http.Request) {
 	// perform some logic
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello again"
 
+	// get the ip from the session and add it to the stringMap
+	remoteIP := rs.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
 	// send the template to the front end
 	render.RenderTemplate(responseWriter, "about.page.gohtml", &models.TemplateData{StringMap: stringMap})
 }
